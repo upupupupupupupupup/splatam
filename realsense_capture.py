4edfa3ed4cf4d2ef72a274bc72ea791ea27618f4
Chenyu Yang <3133257065@qq.com>
2024-08-25 11:15:36 +0800

    [ycy] add mmpose

---
+++ realsense_capture.py
@@ -0,0 +1,144 @@
+## License: Apache 2.0. See LICENSE file in root directory.
+## Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+
+#####################################################
+##              Align Depth to Color               ##
+#####################################################
+
+import os
+# First import the library
+import pyrealsense2 as rs
+# Import Numpy for easy array manipulation
+import numpy as np
+# Import OpenCV for easy image rendering
+import cv2
+
+def save_depth_frame(img, timestamp):
+    save_image_dir = os.path.join(os.getcwd(), "depth")
+    if not os.path.exists(save_image_dir):
+        os.mkdir(save_image_dir)
+    raw_filename = save_image_dir + "/{}.png".format(timestamp)
+    #data.tofile(raw_filename)
+    cv2.imwrite(raw_filename, img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+
+def save_color_frame(img, timestamp):
+    save_image_dir = os.path.join(os.getcwd(), "rgb")
+    if not os.path.exists(save_image_dir):
+        os.mkdir(save_image_dir)
+    filename = save_image_dir + "/{}.png".format(timestamp)
+    if img is None:
+        print("failed to convert frame to image")
+        return
+    cv2.imwrite(filename, img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+
+def main():
+    # Create a pipeline
+    pipeline = rs.pipeline()
+
+    # Create a config and configure the pipeline to stream
+    #  different resolutions of color and depth streams
+    config = rs.config()
+
+    # Get device product line for setting a supporting resolution
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+    found_rgb = False
+    for s in device.sensors:
+        if s.get_info(rs.camera_info.name) == 'RGB Camera':
+            found_rgb = True
+            break
+    if not found_rgb:
+        print("The demo requires Depth camera with Color sensor")
+        exit(0)
+
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+    # Start streaming
+    profile = pipeline.start(config)
+
+    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+    print("Depth Scale is: " , depth_scale)
+
+    profile_d = profile.get_stream(rs.stream.depth)
+    profile_c = profile.get_stream(rs.stream.color)
+    intr_d = profile_d.as_video_stream_profile().get_intrinsics()
+    intr_c = profile_c.as_video_stream_profile().get_intrinsics()
+    print("深度传感器内参：", intr_d)
+    print("RGB相机内参:", intr_c)
+
+    # We will be removing the background of objects more than
+    #  clipping_distance_in_meters meters away
+    clipping_distance_in_meters = 1 #1 meter
+    clipping_distance = clipping_distance_in_meters / depth_scale
+
+    # Create an align object
+    # rs.align allows us to perform alignment of depth frames to others frames
+    # The "align_to" is the stream type to which we plan to align depth frames.
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+
+    saved_cnt: int = 0
+    num: int = 1
+
+    # Streaming loop
+    try:
+        while True:
+            # Get frameset of color and depth
+            frames = pipeline.wait_for_frames()
+            # frames.get_depth_frame() is a 640x360 depth image
+
+            # Align the depth frame to color frame
+            aligned_frames = align.process(frames)
+
+            # Get aligned frames
+            aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+            color_frame = aligned_frames.get_color_frame()
+
+            # Validate that both frames are valid
+            if not aligned_depth_frame or not color_frame:
+                continue
+
+            depth_image = np.asanyarray(aligned_depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+
+            timestamp = "%.3f" % (aligned_depth_frame.get_timestamp() / 1000)
+
+            if saved_cnt < 50:
+                if num % 6 == 0:
+                    save_depth_frame(depth_image, timestamp)
+                    save_color_frame(color_image, timestamp)
+                    saved_cnt += 1
+            else:
+                print("\nget_all_image")
+                break
+            num += 1
+
+            # Remove background - Set pixels further than clipping_distance to grey
+            grey_color = 153
+            depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+            bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+
+            # Render images:
+            #   depth align to color on left
+            #   depth on right
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            images = np.hstack((bg_removed, depth_colormap))
+
+            cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
+            cv2.imshow('Align Example', images)
+            key = cv2.waitKey(1)
+            # Press esc or 'q' to close the image window
+            if key & 0xFF == ord('q') or key == 27:
+                cv2.destroyAllWindows()
+                break
+    finally:
+        pipeline.stop()
+
+if __name__ == "__main__":
+    main()
